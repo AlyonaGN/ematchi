ca671a390cc246f295cd55485175484622f07d89
Alena <al.gnevusheva@gmail.com>
2024-10-25 12:47:44 +0200

    build grid board

---
+++ src/routes/Game.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Grid from './Grid.svelte';
+</script>
+
+<div class="game">
+	<div class="info" />
+	<div class="grid-container">
+		<Grid />
+	</div>
+	<div class="info" />
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		font-size: min(1vmin, 0.3rem);
+	}
+
+	.info {
+		width: 80vmin;
+		height: 10vmin;
+		background-color: purple;
+	}
+
+	.grid-container {
+		width: 80vmin;
+		height: 80vmin;
+		background-color: teal;
+	}
+</style>
