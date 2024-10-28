b57568f356ca30a63b704d958b955b991aa73b93
Alena <al.gnevusheva@gmail.com>
2024-10-28 15:01:17 +0100

    replace emoji with twemoji, add transitions and improve tyles

--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
+	import { get_twemoji_url } from './utils';
+
 	export let emoji: string = '';
 	export let selected: boolean;
 	export let found: boolean;
 </script>
 
-<div class="square" class:flipped={selected}>
+<div class="square" class:flipped={selected || found}>
 	<button on:click />
+	<div class="background" />
 	{#if !found}
-		<span>
-			{emoji}
-		</span>
+		<img alt={emoji} src={get_twemoji_url(emoji)} />
 	{/if}
 </div>
 
@@ -19,20 +20,41 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		transition: transform 0.4s;
+		transform-style: preserve-3d;
+	}
+
+	.flipped {
+		background-color: rosybrown;
+		transform: rotateY(180deg);
+	}
+
+	.background {
+		transform: rotateY(180deg);
+		backface-visibility: hidden;
+		background-color: white;
+		border: 0.5em solid #eee;
+		width: 100%;
+		height: 100%;
+		position: absolute;
 	}
 
 	button {
 		width: 100%;
 		height: 100%;
 		position: absolute;
+		backface-visibility: hidden;
+		background-color: #eee;
+		border: none;
+		border-radius: 1em;
+		font-size: inherit;
 	}
 
-	.flipped button {
-		background-color: rosybrown;
-	}
-
-	span {
-		font-size: 5rem;
+	img {
+		width: 6em;
+		height: 6em;
 		pointer-events: none;
+		transform: rotateY(180deg);
+		backface-visibility: hidden;
 	}
 </style>
