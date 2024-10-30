81b5f0f21e1e9205c8be57abb4788e1e34040822
Alena <al.gnevusheva@gmail.com>
2024-10-30 12:04:42 +0100

    polish styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import '../styles.css';
 	import Modal from './Modal.svelte';
 	import { levels } from './levels';
+	import { confetti } from '@neoconfetti/svelte';
 
 	let state: 'waiting' | 'playing' | 'paused' | 'won' | 'lost' = 'waiting';
 	let game: Game;
@@ -41,13 +42,15 @@
 
 		<div class="buttons">
 			{#if state === 'paused'}
-				<button on:click={() => {}}>resume</button>
-				<button on:click={() => {}}>quit</button>
+				<button on:click={() => game.resume()}>resume</button>
+				<button on:click={() => (state = 'waiting')}> quit </button>
 			{:else}
 				{#each levels as level}
 					<button
+						class="level-button"
 						on:click={() => {
 							game.start(level);
+							state = 'playing';
 						}}>{level.label}</button
 					>
 				{/each}
@@ -56,9 +59,20 @@
 	</Modal>
 {/if}
 
+{#if state === 'won'}
+	<div
+		class="confetti"
+		use:confetti={{
+			stageWidth: innerWidth,
+			stageHeight: innerHeight
+		}}
+	/>
+{/if}
+
 <style>
 	h1 {
 		font-size: 4em;
+		text-align: center;
 	}
 
 	h1 span {
@@ -67,5 +81,38 @@
 
 	p {
 		font-family: Grandstander;
+		font-size: 1.5em;
+		text-align: center;
+	}
+
+	.confetti {
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		top: 30%;
+		left: 50%;
+		pointer-events: none;
+	}
+
+	.buttons {
+		margin-top: 40px;
+		display: flex;
+		gap: 1em;
+	}
+
+	button {
+		font-size: 2em;
+		border-radius: 16px;
+		background-color: rgb(91, 27, 27);
+		color: white;
+		border: none;
+		padding: 20px 20px;
+		cursor: pointer;
+	}
+
+	button:hover {
+		color: rgb(91, 27, 27);
+		background-color: white;
+		border: rgb(91, 27, 27) 1px solid;
 	}
 </style>
